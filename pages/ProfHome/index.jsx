2e96af5fd381d031189643b4ac5798ddab714ac8
Amin Kiani <100538655+M-Amin-Kiani@users.noreply.github.com>
2024-04-11 20:47:10 +0330

    Add files via upload

--- pages/ProfHome/index.jsx
+++ pages/ProfHome/index.jsx
@@ -1,11 +1,17 @@
 import Courses from "@/components/courses";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const profHome = () => {
   return(
       <div className={'h-screen w-screen bg-white'}>
           <div>
               <Header/>
+          </div>
+          <div className={'text-white'}>
+            <a className={''}>
+                درس های درحال انتخاب
+            </a>
           </div>
           <Courses/>
            <div>
