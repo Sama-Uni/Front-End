86c1197c603e792a5e4de3ee95a4ef0ec828b5c3
nima nekouei nia <nekoueinima@gmail.com>
2024-05-18 01:16:04 +0330

    move project to new repository

--- components/courses/index.jsx
+++ components/courses/index.jsx
@@ -1,19 +1,32 @@
-import {useState} from "react";
+import { useState } from "react";
 import Course from "@/components/course";
 
-const Courses = () => {
-    const [cours , setCours]=useState([{name:'mabani' ,term:'2',TANum:'8',requestNum:'10',accepted:"2"},
-        {name:'mabani' ,term:'2',TANum:'8',requestNum:'10',accepted:"2"},
-        {name:'mabani' ,term:'2',TANum:'8',requestNum:'10',accepted:"2"},{name:'mabani' ,term:'2',TANum:'8',requestNum:'10',accepted:"2"},
-        {name:'mabani' ,term:'2',TANum:'8',requestNum:'10',accepted:"2"},])
-  return(
-      <div className={' grid grid-cols-4 gap-4'}>
-          {cours.map((k,index)=>
-                      <div>
-                  <Course name={k.name} term={k.term} TANum={k.TANum} requestNum={k.requestNum} accepted={k.accepted} />
-              </div>
-              )}
-      </div>
-  )
-}
+const Courses = ({ courses }) => {
+  // const [cours, setCours] = useState([
+  //   {
+  //     name: "مبانی هوش",
+  //     term: "5",
+  //     TANum: "8",
+  //     requestNum: "10",
+  //     accepted: "6",
+  //   },
+  // ]);
+  return (
+    <div className={" grid grid-cols-4 gap-4"}>
+      {courses?.map((k, index) => (
+        <div>
+          <Course
+            key={`course-${index}`}
+            id={k.id}
+            name={k.name}
+            term={k.term}
+            TANum={k.required_TAs}
+            //requestNum={0}
+            //accepted={0}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Courses;
